d0e449f35f4794317d41ddfffe78cc06ae31f320
Hamza Nouali <nouali.hamza.1998@gmail.com>
2021-08-08 18:11:37 -0700

    components - wip: fix toggle in config cards

--- stories/Components/Config Cards/index.js
+++ stories/Components/Config Cards/index.js
@@ -87,15 +87,8 @@ export const createConfigCards = ({}) => {
   const div = document.createElement('div')
   div.innerHTML = configCardsElement
 
-  document.querySelectorAll('.ui-config-cards__card input.ui-switch').forEach(function (switchInput) {
-    var switchCard = switchInput.closest('.ui-config-cards__card')
-    switchInput.onchange = function (event) {
-      if (switchInput.checked) {
-        switchCard.classList.add('ui-config-cards__card--active')
-      } else {
-        switchCard.classList.remove('ui-config-cards__card--active')
-      }
-    }
+  setTimeout(() => {
+    setupConfigCards(div.firstElementChild)
   })
 
   return div.firstElementChild
